55bc35979decb8b50058636b78c9b2ba8172558c
Vitaliy Semenov <synthex@bk.ru>
2023-06-14 14:03:03 +0600

    feat: add a private (editor) endpoint to update a travel

--- laravel-travel-agency-api/app/Http/Controllers/API/v1/Admin/TravelController.php
+++ laravel-travel-agency-api/app/Http/Controllers/API/v1/Admin/TravelController.php
@@ -17,4 +17,11 @@ class TravelController extends Controller
 
         return new TravelResource($travel);
     }
+
+    public function update(Travel $travel, TravelRequest $request): JsonResource
+    {
+        $travel->update($request->validated());
+
+        return new TravelResource($travel);
+    }
 }
--- laravel-travel-agency-api/routes/api.php
+++ laravel-travel-agency-api/routes/api.php
@@ -22,9 +22,13 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('travels', TravelController::class);
 Route::get('travels/{travel:slug}/tours', [TourController::class, 'index']);
 
-Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function () {
-    Route::post('travels', [Admin\TravelController::class, 'store']);
-    Route::post('travels/{travel}/tours', [Admin\TourController::class, 'store']);
+Route::prefix('admin')->middleware('auth:sanctum')->group(function () {
+    Route::middleware('role:admin')->group(function() {
+        Route::post('travels', [Admin\TravelController::class, 'store']);
+        Route::post('travels/{travel}/tours', [Admin\TourController::class, 'store']);
+    });
+
+    Route::put('travels/{travel}', [Admin\TravelController::class, 'update']);
 });
 
 Route::post('auth/login', LoginController::class);
